9c41360e699df7897647beedd6558350a4bbe35f
nikhilyadavvvv <nikhilyadavvvv@gmail.com>
2023-02-28 12:42:13 +0100

    Updates

---
+++ precache-manifest.a4ae501d4f09705d4bca6d98e434d652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7c0fb23c2d1bed0618e5efb79966a3",
+    "url": "/react-image-mapper/index.html"
+  },
+  {
+    "revision": "9965b58a39e437ba5d6c",
+    "url": "/react-image-mapper/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c39b9c8d3ae4d35e52a7",
+    "url": "/react-image-mapper/static/js/2.1017149e.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/react-image-mapper/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "9965b58a39e437ba5d6c",
+    "url": "/react-image-mapper/static/js/main.15f13c31.chunk.js"
+  },
+  {
+    "revision": "6f75e29a78f2755ef6c3",
+    "url": "/react-image-mapper/static/js/runtime~main.40afabd3.js"
+  },
+  {
+    "revision": "7eb8d98b4c14691ae7997610de63ab22",
+    "url": "/react-image-mapper/static/media/museum_poster.7eb8d98b.png"
+  }
+]);
